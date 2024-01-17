ea6bd1587c01ebfa75b0d4b1de8970d7be46093c
Mahlet Seleshi <mahletseleshi224@gmail.com>
2024-01-17 21:55:57 +0300

    updated page

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import {Outlet, Navigate} from "react-router-dom";
+import sideImg from "../assets/images/sideImg.svg";
+
 
 const AuthLayout = () => {
-  return (
+  const isAuth = false; 
+
+
+  return ( 
     <div>
+      <>
+     {isAuth?(
+      <Navigate to = "/"/>
+     ) :
+     <>
+     <section className="flex flex-1 items-center justify-center flex-col py-10">
+      <Outlet />
+     </section>
+
+     <img
+            src={sideImg}
+            alt="logo"
+            className="hidde lg:block h-screen w-1/2"
+          />
+
+     
+     </>
+     
+     }      
+      </>
       
     </div>
   )
