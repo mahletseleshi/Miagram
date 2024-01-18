49ea7b6944f4f0df9a5fe01729cf967554c12d28
Mahlet Seleshi <mahletseleshi224@gmail.com>
2024-01-18 14:46:34 +0300

    updated page

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,36 +1,27 @@
 import {Outlet, Navigate} from "react-router-dom";
-import sideImg from "../assets/images/sideImg.svg";
 
 
 const AuthLayout = () => {
-  const isAuth = false; 
+  const isAuthenticated = false; 
+  return (
+    <>
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ) : (
+        <>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
+            <Outlet />
+          </section>
 
-
-  return ( 
-    <div>
-      <>
-     {isAuth?(
-      <Navigate to = "/"/>
-     ) :
-     <>
-     <section className="flex flex-1 items-center justify-center flex-col py-10">
-      <Outlet />
-     </section>
-
-     <img
-            src={sideImg}
+          <img
+            src="/assets/images/side-img.svg"
             alt="logo"
-            className="hidde lg:block h-screen w-1/2"
+            className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
           />
-
-     
-     </>
-     
-     }      
-      </>
-      
-    </div>
-  )
+        </>
+      )}
+    </>
+  );
 }
 
 export default AuthLayout
